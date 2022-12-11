e94201986b7acae1f8ee967f229ed9e001ae6503
Latika04Parolkar <60360732+Latika04Parolkar@users.noreply.github.com>
2022-12-12 02:23:42 +0530

    DB uri added

--- config/db.js
+++ config/db.js
@@ -1,30 +1,19 @@
-// const Sequelize = require('sequelize');
-// const env = require('dotenv');
-// env.config();
+const Sequelize = require('sequelize');
+const env = require('dotenv');
+env.config();
 
-// const sequelize = new Sequelize("yoga", "root", "", {
-//     dialect: "mysql",
-//     host: process.env.HOST,
-// }, {
-//     pool: {
-//         max: process.env.POOL_MAX,
-//         min: process.env.POOL_MIN,
-//         acquire: process.env.POOL_ACQUIRE,
-//         idle: process.env.POOL_IDLE
-//     }
-// });
+const sequelize = new Sequelize(process.env.DB_URI);
 
-
-// (async () => {
-//     try {
-//         await sequelize.authenticate();
-//         console.log("Database Connected!");
-//     } catch (error) {
-//         console.log("Couldn't Connect to server!");
-//         return "Server Error";
-//     }
-// })();
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Database Connected!");
+    } catch (error) {
+        console.log("Couldn't Connect to server!");
+        return "Server Error";
+    }
+})();
 
 
 
-// module.exports = sequelize;
+module.exports = sequelize;
